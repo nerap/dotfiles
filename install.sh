9316740179c2a9ad397243c5cbe4d0be9e8423b6
nerap <44852526+nerap@users.noreply.github.com>
2024-07-21 22:42:59 +0200

    ⚡️ added arc, rust, nvm
    Signed-off-by: nerap <44852526+nerap@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,80 +1,85 @@
 #!/usr/bin/env zsh
 
-
 # Install Homebrew
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Homebrew packages
-# brew install \
-#    autoconf \
-#    automake \
-#    bash \
-#    bash-completion \
-#    bat \
-#    cmake \
-#    coreutils \
-#    ctags \
-#    curl \
-#    diff-so-fancy \
-#    docker \
-#    fd \
-#    font-hack-nerd-font \
-#    fzf \
-#    gcc \
-#    git \
-#    git-delta \
-#    gnu-sed \
-#    gnu-tar \
-#    gnu-which \
-#    gpg \
-#    grep \
-#    htop \
-#    jq \
-#    less \
-#    libtool \
-#    lsd \
-#    luarocks \
-#    luv \
-#    make \
-#    neovim \
-#    node \
-#    openssh \
-#    openssl \
-#    p7zip \
-#    pandoc \
-#    python \
-#    python@3.9 \
-#    ripgrep \
-#    ruby \
-#    stripe-cli \
-#    supabase \
-#    sqlite \
-#    tmux \
-#    tree-sitter \
-#    tree-sitter-lua \
-#    tree-sitter-python \
-#    tree-sitter-typescript \
-#    tree-sitter-yaml \
-#    unibilium \
-#    unzip \
-#    wget \
-#    xz \
-#    yarn \
-#    zsh
-
+brew install \
+   autoconf \
+   automake \
+   bash \
+   bash-completion \
+   bat \
+   cmake \
+   coreutils \
+   ctags \
+   curl \
+   diff-so-fancy \
+   docker \
+   fd \
+   font-hack-nerd-font \
+   fzf \
+   gcc \
+   git \
+   git-delta \
+   gnu-sed \
+   gnu-tar \
+   gnu-which \
+   gpg \
+   grep \
+   htop \
+   jq \
+   less \
+   libtool \
+   lsd \
+   luarocks \
+   luv \
+   make \
+   nvm \
+   neovim \
+   node \
+   openssh \
+   openssl \
+   p7zip \
+   pandoc \
+   python \
+   python@3.9 \
+   ripgrep \
+   ruby \
+   stripe-cli \
+   supabase \
+   sqlite \
+   tmux \
+   tree-sitter \
+   tree-sitter-lua \
+   tree-sitter-python \
+   tree-sitter-typescript \
+   tree-sitter-yaml \
+   unibilium \
+   unzip \
+   wget \
+   xz \
+   yarn \
+   zsh
 
 # Install Homebrew casks
 
-# brew install --cask \
-#   discord \
-#   docker \
-#   dbeaver-community
-#   google-chrome \
-#   iterm2 \
-#   notion \
-#   slack \
-#   spotify \
-#   visual-studio-code \
+brew install --cask \
+  arc \
+  discord \
+  docker \
+  dbeaver-community
+  obsidian \
+  iterm2 \
+  notion \
+  slack \
+  spotify
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install Node
+nvm install node
 
 ## Symbolic links
 # ZSH
@@ -90,9 +95,10 @@ ln -sf ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
 
 # Neovim
-#git clone https://github.com/nerap/nvim ~/.dotfiles/nvim
-#git clone https://github.com/ThePrimeagen/harpoon.git ~/personal/harpoon -b harpoon2
-#ln -sf ./nvim ~/.config
+git clone https://github.com/nerap/nvim ~/.dotfiles/nvim
+git clone https://github.com/ThePrimeagen/harpoon.git ~/personal/harpoon -b harpoon2
+git clone https://github.com/nerap/gitmoji.nvim.git ~/personal/gitmoji
+ln -sf ./nvim ~/.config
 
 # Local
 chmod +x ~/.dotfiles/bin/.local/scripts/*
