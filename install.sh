3d205d03df1463f444399a0c568dec9e231ef83a
nerap <44852526+nerap@users.noreply.github.com>
2024-08-25 01:36:09 +0200

    ✨ desactivate SIP
    Signed-off-by: nerap <44852526+nerap@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -132,6 +132,7 @@ echo ""
 echo "----- setup mac system -----"
 
 sudo -v
+csrutil disable
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
